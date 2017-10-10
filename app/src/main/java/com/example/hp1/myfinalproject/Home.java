1f5a4496e972aae7fcb87584afe8fae72f99aee0
Hp1 <hu_sam_2000@hotmail.com>
2017-10-10 12:42:27 +0300

    notification

--- app/src/main/java/com/example/hp1/myfinalproject/Home.java
+++ app/src/main/java/com/example/hp1/myfinalproject/Home.java
@@ -1,4 +1,6 @@
 package com.example.hp1.myfinalproject;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
@@ -6,6 +8,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.NotificationCompat;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -26,6 +29,30 @@ public class Home  extends AppCompatActivity implements View.OnClickListener,Ada
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        btnNotify = (Button) findViewById(R.id.btnNotify);
+
+        //create builder object
+        builder = new NotificationCompat.Builder(this);
+
+        //customize the builder
+        builder.setSmallIcon(R.mipmap.ic_launcher);
+        builder.setContentTitle("Notification Title");
+        builder.setContentText("this is the body of the notification message");
+
+        //
+        Intent bIntent = new Intent(this, about.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(this, 0, bIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        builder.setContentIntent(contentIntent);
+
+        btnNotify.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Add as notification
+                NotificationManager manager = (NotificationManager) getSystemService(getApplicationContext().NOTIFICATION_SERVICE);
+                manager.notify(0, builder.build());
+            }
+        });
+
         //  btrelationship=(Button)findViewById(R.id.btcouple);
         //     btask=(Button)findViewById(R.id.btask);
         //btrelationship.setOnClickListener(this);
@@ -94,4 +121,11 @@ return true;
         AlertDialog alert = builder.create();
         alert.show();
     }
-}
+    private NotificationCompat.Builder builder;
+    private Button btnNotify;
+
+
+
+    }
+
+
